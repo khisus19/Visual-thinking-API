b15caad775f53f082498f7f6bde792bf9741e679
Jesús Álvarez <khisus19@gmail.com>
2022-05-10 21:21:45 -0500

    9. Prueba de tercer método failed

--- test/services/StudentService.test.js
+++ test/services/StudentService.test.js
@@ -36,4 +36,22 @@ describe("Pruebas para StudentService", () => {
         const certificationChecklist = StudentService.certificationCheck(students);
         expect(certificationChecklist[0].haveCertification).toBeTruthy();
     });
+
+    test("Método 3: Listar los estudiantes que tengan credits mayor a 500", () =>{
+        const students = [
+            {
+                "id": "6264d5d89f1df827eb84bb23",
+                "name": "Warren",
+                "email": "Todd@visualpartnership.xyz",
+                "credits": 508,
+                "enrollments": [
+                    "Visual Thinking Intermedio",
+                    "Visual Thinking Avanzado"
+                ],
+                "previousCourses": 1,
+                "haveCertification": true
+            }];
+        const bigCreditlist = StudentService.bigCredits(students);
+        expect(bigCreditlist[0].credits > 500).toBeTruthy();
+    });
 });
