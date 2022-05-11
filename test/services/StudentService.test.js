fe8f921613a6fa50d0542e3cd9711d49e7c0eee9
Jesús Álvarez <khisus19@gmail.com>
2022-05-10 20:03:54 -0500

    8. Creado el método 2 para listar los students con certification y su prueba

--- test/services/StudentService.test.js
+++ test/services/StudentService.test.js
@@ -1,4 +1,4 @@
-const StudentServices = require("./../../lib/services/StudentService");
+const StudentService = require("./../../lib/services/StudentService");
 
 describe("Pruebas para StudentService", () => {
     test("Método 1: Listar los estudiantes con sus respectivos datos", () => {
@@ -15,6 +15,25 @@ describe("Pruebas para StudentService", () => {
                 "previousCourses": 1,
                 "haveCertification": true
             }];
-        expect(students.name).toBe("Warren");
+        const studentList = StudentService.getStudentList(students);
+        expect(studentList[0].id).toBeDefined();
+    });
+
+    test("Método 2: Listar los estudiantes que tengan haveCertification: true", () =>{
+        const students = [
+            {
+                "id": "6264d5d89f1df827eb84bb23",
+                "name": "Warren",
+                "email": "Todd@visualpartnership.xyz",
+                "credits": 508,
+                "enrollments": [
+                    "Visual Thinking Intermedio",
+                    "Visual Thinking Avanzado"
+                ],
+                "previousCourses": 1,
+                "haveCertification": true
+            }];
+        const certificationChecklist = StudentService.certificationCheck(students);
+        expect(certificationChecklist[0].haveCertification).toBeTruthy();
     });
 });
